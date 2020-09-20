6d7605ee493da49eb54c689528e1582e1f0357fb
baopan <baopan@aixuexi.com>
2020-09-20 19:49:00 +0800

    add web

--- src/main/java/com/currentbp/myspringcloudweb/MyspringcloudwebApplication.java
+++ src/main/java/com/currentbp/myspringcloudweb/MyspringcloudwebApplication.java
@@ -2,14 +2,24 @@ package com.currentbp.myspringcloudweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class MyspringcloudwebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MyspringcloudwebApplication.class, args);
     }
 
+    @LoadBalanced //使用负载均衡机制
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
--- src/main/java/com/currentbp/myspringcloudweb/controller/StudentController.java
+++ src/main/java/com/currentbp/myspringcloudweb/controller/StudentController.java
@@ -1,13 +1,15 @@
 package com.currentbp.myspringcloudweb.controller;
 
+import com.currentbp.myspringcloudapi.modul.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author baopan
@@ -20,9 +22,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/student")
 public class StudentController {
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String home() {
         return "hi ,i am from port:";
     }
+
+
+
+    @GetMapping("/getByName")
+    public String getByName(@RequestParam String name){
+        System.out.println("===>name:"+name);
+        return restTemplate.getForObject("http://myspringcloudservice/getByName?name="+name, String.class);
+    }
+
+    @GetMapping("/getById")
+    public Student getById(@RequestParam Integer id){
+        System.out.println("===>id:"+id);
+        return restTemplate.getForObject("http://myspringcloudservice/getById?id="+id, Student.class);
+    }
+
+
+//    @GetMapping("/getByIds")
+//    public List getByIds(@RequestParam List<Integer> ids){
+//        return restTemplate.getForObject("http://myspringcloudservice/getByIds?ids="+ids, List.class);
+//    }
 
 }
